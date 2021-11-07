816e16b296ffeebd29353201710ac6db8f51bce3
Dominik <babidominik26@gmail.com>
2021-11-07 13:51:06 +0100

    pi.cpp and pi.exe

---
+++ Vjezba/Prvi_lab/pi.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+double pi(int n) {
+    if (n <= 0) return 0;
+
+    double numPi = pi(n - 1) + 4 * pow(-1, n + 1) / (2*n - 1);
+    return numPi ;
+}
+
+int main(void) {
+    int n;
+    cout << "Unesi n ";
+    cin >> n;
+    
+    double *A = new double[n];
+    for (int i = 0; i < n; i++) {
+        A[i] = pi(i + 1);
+        cout << A[i] << endl;
+    }
+}
